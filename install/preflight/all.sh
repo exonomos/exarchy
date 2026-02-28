a903da54e6e182d7c768cf52b369bd0384d501b2
exonomos <git.exonomos.de>
2026-02-28 18:32:10 +0100

    refactor: moving loadkeys.sh & timedatectl.sh to preflight

--- install/preflight/all.sh
+++ install/preflight/all.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+source "$EXARCHY_INSTALL/preflight/loadkeys.sh"
+source "$EXARCHY_INSTALL/preflight/timedatectl.sh"
+
 #run_logged "$EXARCHY_INSTALL/preflight/uefi-guard.sh"
 #run_logged "$EXARCHY_INSTALL/preflight/network-ping.sh"
 #run_logged "$EXARCHY_INSTALL/preflight/env-check.sh"
