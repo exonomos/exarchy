6593e1b678b90381ee3db73fdab76b7dbe59ddf0
exonomos <git.exonomos.de>
2026-02-28 11:33:51 +0100

    Revert "fix: adding ssh to the installation in pacstrap.sh"
    This reverts commit e4fb00f3693718e92b8c1ca56567e94f55ec37d4.

--- install/base/pacstrap.sh
+++ install/base/pacstrap.sh
@@ -1,2 +1,2 @@
 echo "-> Installing base system via pacstrap..."
-pacstrap -K /mnt base base-devel linux linux-firmware intel-ucode btrfs-progs networkmanager nvim git ssh
+pacstrap -K /mnt base base-devel linux linux-firmware intel-ucode btrfs-progs networkmanager nvim git
